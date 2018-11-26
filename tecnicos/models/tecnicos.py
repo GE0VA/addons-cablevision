a93b69f213c12edfc959d390e0241ac80a6bd04b
franciscomg <fmelendezg@est.utn.ac.cr>
2018-11-26 10:06:56 -0600

    [ADD] Trabajo en el módulo tecnicos

--- tecnicos/models/tecnicos.py
+++ tecnicos/models/tecnicos.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from datetime import datetime, timedelta, date
 
 class Tecnicos(models.Model):
      _name = 'tecnicos.odoo'
@@ -16,7 +17,15 @@ class Tecnicos(models.Model):
      licenses_ids = fields.One2many( comodel_name="tecnicos.odoo.lines", inverse_name="tecnico_id", string="Licencias" )
      titles_ids = fields.Many2many( comodel_name="titulos.odoo", relation="tecnico_odoo_rel", column1="tecnico_id",
                                          column2="titulo_id", string="Titulos" )
-     state = fields.Selection(string="Estado trabajador", selection=[('0', 'Borrador'), ('1', 'Contratado'), ('2', 'Despedido'), ('3', 'Cancelado')], track_visibility='always', default="0" )
+     state = fields.Selection( string="Estado trabajador", selection=[('0', 'Borrador'), ('1', 'Contratado'), ('2', 'Despedido'), ('3', 'Cancelado')], track_visibility='always', default="0" )
+     labor_date = fields.Char( string="Tiempo laborado", compute="_compute_time", store=True )
+     days_add = fields.Integer( string="Mas dias" )
+
+     @api.depends('ingress_date')
+     def _compute_time(self):
+          for rec in self:
+              time = fields.Date.from_string(str(datetime.today())) - fields.Date.from_string(str((rec.ingress_date) or datetime.today()) [:10])
+              rec.time_onbusiness = time +  timedelta(days =0) #self.days_add
 
      @api.multi
      def a_contratado(self):
