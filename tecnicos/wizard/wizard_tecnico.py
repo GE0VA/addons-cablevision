952279bd09f39118308ca120efec13b6973a630e
Andrés Garita <garitamc7@gmail.com>
2018-11-27 16:51:37 -0600

    [ADD] Se agrega módulo Técnico
    Signed-off-by: Andrés Garita <garitamc7@gmail.com>

---
+++ tecnicos/wizard/wizard_tecnico.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class WizardTecnico(models.TransientModel):
+    _name = 'tecnico.wizard'
+
+    user = fields.Many2one(comodel_name="res.users",
+                           string="Usuario a asignar",
+                           required=True,
+                           )
+    date = fields.Date(string="Fecha a revisar",
+                       required=False)
+
+    @api.multi
+    def register_users(self):
+        if self._context.get('active_ids', False):
+            obj_tecnicos = self.env['tecnicos.odoo'].browse(self._context.get('active_ids', False))
+            for rec in obj_tecnicos:
+                rec.date_revision = self.date
+                rec.user = self.user.id
